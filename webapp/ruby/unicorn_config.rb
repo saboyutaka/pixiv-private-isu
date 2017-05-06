1536afc830d5e2716a36d915624147c65fc34f66
Yutaka Tachibana <yutaka.paradigm.shift@gmail.com>
2017-05-06 18:52:06 +0900

    unicorn

--- webapp/ruby/unicorn_config.rb
+++ webapp/ruby/unicorn_config.rb
@@ -1,7 +1,7 @@
 worker_processes 4
 preload_app true
-# listen "127.0.0.1:9292"
-listen "/tmp/unicorn.sock"
+listen "127.0.0.1:8080"
+# listen "/tmp/unicorn.sock"
 pid "/tmp/unicorn.pid"
 # stderr_path "/tmp/unicorn_error.log"
 # stdout_path "/tmp/unicorn.log"
