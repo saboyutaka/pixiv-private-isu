33063c1506322f0b6b9277873c7c2d4b1e374a43
Yutaka Tachibana <yutaka.paradigm.shift@gmail.com>
2017-05-06 13:37:46 +0900

    pumaのportを変更

--- webapp/ruby/config/puma.rb
+++ webapp/ruby/config/puma.rb
@@ -4,7 +4,7 @@ workers Integer(ENV['WEB_CONCURRENCY'] || 4)
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
-# bind 'tcp://0.0.0.0:9292'
+bind 'tcp://0.0.0.0:8080'
 # bind 'unix:///var/run/puma.sock'
 # port        ENV['PORT']     || 8080
 environment ENV['RACK_ENV'] || 'development'
